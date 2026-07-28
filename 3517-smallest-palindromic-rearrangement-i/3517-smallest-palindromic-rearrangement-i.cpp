846873c295e87d24993c71d3330a47b64a94f6f3
RUDRA PRATAP SINGH <rudra.pratapsingha.rp@gmail.com>
2026-07-28 14:03:12 +0530

    Time: 9 ms (98.68%), Space: 75.8 MB (33.63%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        // int n = s.size();
+        // sort(s.begin(),s.begin()+n/2);
+        // sort(s.begin()+n/2,s.end(),greater<char>());
+        // //reverse(s.begin()+n/2,s.end());
+        // return s;
+
+        vector<int>freq(26,0);
+        string left;
+        string right;
+        for(char c:s){
+            freq[c-'a']++;
+        }
+        char mid = 0;
+        for(int i=0;i<26;i++){
+            left.append(freq[i]/2,'a'+i);
+            //appends char 'a'+i freq/2 times
+
+            if(freq[i]%2)
+                mid = 'a'+i;
+
+
+        }
+
+        right = left;
+        reverse(right.begin(),right.end());
+
+        return left+(mid?string(1,mid):"")+right;
+    }
+};
