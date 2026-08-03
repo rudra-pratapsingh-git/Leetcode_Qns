563bf7f95d8da0c76c5ac2d0a357ba6384e81884
RUDRA PRATAP SINGH <rudra.pratapsingha.rp@gmail.com>
2026-08-03 09:46:45 +0530

    Time: 36 ms (7.48%), Space: 21.1 MB (6.91%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,44 +1,135 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+            vector<vector<pair<int,int>>> adj(n);
 
-        for(auto it: edges){
-            dist[it[0]][it[1]] = it[2];
-            dist[it[1]][it[0]]  = it[2];
-        }
+            for(auto edge: edges){
+                adj[edge[0]].push_back({edge[1],edge[2]});
+                adj[edge[1]].push_back({edge[0],edge[2]});
+            }
 
-        for(int i=0;i<n;i++){
-            dist[i][i] = 0;
-        }
+            int cntCity = n;
+            int cityNo = -1;
 
-        for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][k]==INT_MAX || dist[j][k]==INT_MAX)continue;
+                vector<int> dist(n,INT_MAX);
 
-                    dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
-                }
+                priority_queue<pair<int,int>,
+                                vector<pair<int,int>>,
+                                greater<pair<int,int>>> pq;
+
+                dist[i] = 0;
+                pq.push({0,i});
+
+                while(!pq.empty()){
+                    auto it = pq.top();
+                    pq.pop();
+                    int node = it.second;
+                    int dis = it.first;
+
+                    if(dis>dist[node]) continue;
+
+                    for(auto iter: adj[node]){
+                        int edW = iter.second;
+                        int adjNode = iter.first;
+
+                        if(edW + dis < dist[adjNode]){
+                            dist[adjNode] = edW + dis;
+
+                            pq.push({edW+dis,adjNode});
+                        }
+                    }
+
+                }                
+                    int cnt = 0;
+
+                    for(int adjCity = 0;adjCity<n;adjCity++){
+                        if(dist[adjCity]<=distanceThreshold){
+                            cnt++;
+                        }
+                    }
+
+                    if(cnt<=cntCity){
+                        cntCity = cnt;
+                        cityNo = i;
+                    }
             }
-        }
 
-        int cityNo = -1;
-        int cntCity = n;
+            return cityNo;
 
-        for(int city=0;city<n;city++){
-            int cnt = 0;
-            for(int adjCity = 0;adjCity<n;adjCity++){
-                if(dist[city][adjCity]<=distanceThreshold){
-                    cnt++;
-                }
-            }
 
-            if(cnt<=cntCity){
-                cntCity = cnt;
-                cityNo = city;
-            }
-        }
 
-        return cityNo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+
+        // for(auto it: edges){
+        //     dist[it[0]][it[1]] = it[2];
+        //     dist[it[1]][it[0]]  = it[2];
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     dist[i][i] = 0;
+        // }
+
+        // for(int k=0;k<n;k++){
+        //     for(int i=0;i<n;i++){
+        //         for(int j=0;j<n;j++){
+        //             if(dist[i][k]==INT_MAX || dist[j][k]==INT_MAX)continue;
+
+        //             dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
+        //         }
+        //     }
+        // }
+
+        // int cityNo = -1;
+        // int cntCity = n;
+
+        // for(int city=0;city<n;city++){
+        //     int cnt = 0;
+        //     for(int adjCity = 0;adjCity<n;adjCity++){
+        //         if(dist[city][adjCity]<=distanceThreshold){
+        //             cnt++;
+        //         }
+        //     }
+
+        //     if(cnt<=cntCity){
+        //         cntCity = cnt;
+        //         cityNo = city;
+        //     }
+        // }
+
+        // return cityNo;
     }
 };
