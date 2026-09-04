4d3e9282a63849f1a07b4d551725375f1a0ccf33
RUDRA PRATAP SINGH <rudra.pratapsingha.rp@gmail.com>
2026-09-04 17:11:55 +0530

    Time: 4 ms (99.52%), Space: 160 MB (36.09%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,50 +1,76 @@
 class Solution {
 
-    public void reverse(int [] nums, int l,int r){
-        r--;
-        while(l<r){
-            int temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
+    // public void reverse(int [] nums, int l,int r){
+    //     r--;
+    //     while(l<r){
+    //         int temp = nums[l];
+    //         nums[l] = nums[r];
+    //         nums[r] = temp;
 
-            l++;
-            r--;
-        }
-    }
+    //         l++;
+    //         r--;
+    //     }
+    // }
 
 
-    public void rotate(int [] nums,int first,int mid,int last){
-        reverse(nums,first,mid);
-        reverse(nums,mid,last);
-        reverse(nums,first,last);
-    }
-    public int stablePartition(int [] nums, int left,int right, int pivot){
-        if(right - left <=1 ){
-            if(right-left == 1 && nums[left] <= pivot){
-                return 1;
-            }
-            return 0;
-        }
+    // public void rotate(int [] nums,int first,int mid,int last){
+    //     reverse(nums,first,mid);
+    //     reverse(nums,mid,last);
+    //     reverse(nums,first,last);
+    // }
+    // public int stablePartition(int [] nums, int left,int right, int pivot){
+    //     if(right - left <=1 ){
+    //         if(right-left == 1 && nums[left] <= pivot){
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
 
-        int mid = left + (right - left )/2;
+    //     int mid = left + (right - left )/2;
 
-        int leftCount = stablePartition(nums,left,mid,pivot);
-        int rightCount = stablePartition(nums,mid,right,pivot);
+    //     int leftCount = stablePartition(nums,left,mid,pivot);
+    //     int rightCount = stablePartition(nums,mid,right,pivot);
 
-        int first = left + leftCount;
-        int second = mid;
-        int third = mid + rightCount;
+    //     int first = left + leftCount;
+    //     int second = mid;
+    //     int third = mid + rightCount;
 
-        rotate(nums,first,second,third);
+    //     rotate(nums,first,second,third);
 
-        return leftCount + rightCount;
-    }
+    //     return leftCount + rightCount;
+    // }
     public int[] pivotArray(int[] nums, int pivot) {
         int n = nums.length;
 
-        int lessCount = stablePartition(nums,0,n,pivot-1);
-        stablePartition(nums,lessCount,n,pivot);
+        // int lessCount = stablePartition(nums,0,n,pivot-1);
+        // stablePartition(nums,lessCount,n,pivot);
 
-        return nums;
+        // return nums;
+
+        int ans[] = new int[n];
+
+        int lessI = 0;
+        int greaterI = n-1;
+
+        for(int i=0,j=n-1;i<n;i++,j--){
+            if(nums[i] < pivot){
+                ans[lessI] = nums[i];
+                lessI++;
+            }
+
+            if(nums[j] > pivot){
+                ans[greaterI] = nums[j];
+                greaterI--;
+            }
+        }
+
+        while(lessI <= greaterI){
+            ans[lessI] = pivot;
+            lessI++;
+        }
+
+        return ans;
+
+
     }
 }
