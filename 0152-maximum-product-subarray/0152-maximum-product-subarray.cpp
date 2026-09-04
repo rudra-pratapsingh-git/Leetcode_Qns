11a60706d7c1cd9b4342ed15d95a58f1a6e17908
RUDRA PRATAP SINGH <rudra.pratapsingha.rp@gmail.com>
2026-09-04 19:02:53 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.34%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n = nums.size();
+        int maxProd = INT_MIN;
+
+        int prefixProd = 1;
+        int suffixProd = 1;
+
+        for(int i=0;i<n;i++){
+            if(prefixProd == 0){
+                prefixProd = 1;
+            }
+            if(suffixProd == 0){
+                suffixProd = 1;
+            }
+
+            prefixProd *= nums[i];
+            suffixProd *= nums[n-i-1];
+
+            maxProd = max(maxProd,max(prefixProd,suffixProd));
+        }
+
+        return maxProd;
+    }
+};
